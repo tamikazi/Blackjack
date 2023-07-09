2180b8136c6d880b7b8d37b541ead6e3d8c87b35
tamikazi <tahmidkazi829@gmail.com>
2023-07-08 22:59:58 -0600

    added functionality

--- index.js
+++ index.js
@@ -1,14 +1,72 @@
-let firstCard = 5
-let secondCard = 11
 
-let sum = firstCard + secondCard
+let cards = []
 
-if (sum < 21) {
-    console.log("Do you want to draw a new card?")
+let sum = 0
+
+let isAlive = false
+let hasBJ = false
+
+let message = ""
+
+let messageEl = document.getElementById("message-el")
+let cardsEl = document.getElementById("cards-el")
+let sumEl = document.getElementById("sum-el")
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + ", "
+    }
+
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    }
+    else if (sum === 21) {
+        hasBJ = true
+        message = "Blackjack!"
+    }
+    else {
+        isAlive = false
+        message = "Bust!"
+    }
+    messageEl.textContent = message
+
 }
-else if (sum === 21) {
-    console.log("Blackjack!")
+
+function newCard() {
+    if (isAlive === true && hasBJ === false) {
+        let newCard = getRandomCard()
+        sum += newCard
+        cards.push(newCard)
+        renderGame()
+    }
+    
+    
 }
-else if (sum > 21) {
-    console.log("Bust!")
+
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+
 }
+
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+
+    if (randomNumber === 1) {
+        return 11
+    }
+    else if (randomNumber > 10) {
+        return 10
+    }
+    else {
+        return randomNumber
+    }
+}
+
+
